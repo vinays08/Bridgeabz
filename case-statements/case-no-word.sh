164ba026e81f83a376c1f5a239a9786f38fb6506
vinays08 <94815005+vinays08@users.noreply.github.com>
2021-11-23 18:30:21 +0530

    stage-2-review

---
+++ case-statements/case-no-word.sh
@@ -0,0 +1,33 @@
+read -p "Enter a number from 1 to 9: " num
+case $num in
+   1)
+     echo "One"
+     ;;
+   2)
+     echo "Two"
+     ;;
+   3)
+     echo "Three"
+     ;;
+   4)
+     echo "Four"
+     ;;
+   5)
+     echo "Five"
+     ;;
+   6)
+     echo "Six"
+     ;;
+   7)
+     echo "Seven"
+     ;;
+   8)
+     echo "Eight"
+     ;;
+   9)
+     echo "Nine"
+     ;;
+   *)
+     echo "Invalid Input - Enter a number from 1 to 9"
+     ;;
+esac
