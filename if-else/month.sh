386a0b3e48fcce057789eec6336d1790e2ff34ae
vinays08 <94815005+vinays08@users.noreply.github.com>
2021-11-24 03:37:22 +0530

    Update month.sh

--- if-else/month.sh
+++ if-else/month.sh
@@ -1,15 +1,18 @@
-read -p " Enter Date:-" date
-read -p " Enter Month:-" Month
-
-if (( ($Month <= 6 & $date <= 20) ))
+read -p "Enter Date: " date
+read -p "Enter Month: " Month
+condition="false"
+if [ $Month -ge 3 -a $Month -le 6 ]
 then
-        echo $Month $date "True";
-
-elif (( ($Month >= 3 & $Month < 6) & ($date<31)  ))
-then
-        echo $date $Month "True";
-
-else
-
-        echo "False";
+mc=$((30 + (Month % 2)))
+    if [ $date -ge 1 -a $date -le $mc ]
+    then
+        dc=$(((Month*100)+date))
+        if [ $dc -ge 320 -a $dc -le 620 ]
+        then
+            condition="true"
+        fi
+    fi
 fi
+
+echo "$Month/$date $condition"
+
