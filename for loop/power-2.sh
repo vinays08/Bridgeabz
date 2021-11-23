463641c3c88287bb091a860cec01c4bbee274623
vinays08 <94815005+vinays08@users.noreply.github.com>
2021-11-24 04:22:52 +0530

    Update power-2.sh

--- for loop/power-2.sh
+++ for loop/power-2.sh
@@ -1,6 +1,5 @@
-read -p "Enter the nth number:" number
-
-for(( count=0; count<number; count++))
+read -p "Enter the number to print the power of 2 : " number
+for(( count=0; count<=number; count++))
 do
-	echo $((2**count))
+	echo  2 ^ $count = $((2**count))
 done
