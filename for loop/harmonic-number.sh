afda20717cbb0cafb6543932d77eb5b573dc0ddc
vinays08 <94815005+vinays08@users.noreply.github.com>
2021-11-24 04:40:09 +0530

    Update harmonic-number.sh

--- for loop/harmonic-number.sh
+++ for loop/harmonic-number.sh
@@ -1,10 +1,16 @@
-read -p 'enter number ' num
-for (( count = 1 ; count<=$num ; count++ ))
+read -p "Enter value for Harmonic Number: " n
+
+for (( i=1; i<=n; i++ ))
 do
-if [ $count -eq $num ];
-then
-   printf '1/'$count;
-else
-   printf '1/'$count' + ';
-fi
+  if [ $i == 1 ]
+  then
+	h="(1/$i)"
+  elif [ $i -lt $n ]
+  then   
+  	h="$h+(1/$i)"
+  else
+	h="$h+(1/$i)"
+  fi
 done
+echo "Harmonic value for $n is $h"
+
