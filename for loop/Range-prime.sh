a1bead5e9435408aced901cd2d92111752bec77e
vinays08 <94815005+vinays08@users.noreply.github.com>
2021-11-24 04:55:16 +0530

    stage-2-review

---
+++ for loop/Range-prime.sh
@@ -0,0 +1,18 @@
+read -p "Enter the Start number.." start
+read -p "Enter the End number.." end
+for ((i=$start;i<=$end;i++))
+do
+        count=0
+        for((j=1;j<=$i;j++))
+        do
+                if [ $(($i%$j)) -eq 0 ]
+                then
+                count=$(($count+1))
+                fi
+        done
+        if [ $count -eq 2 ]
+        then
+        echo $i
+        fi
+done
+
