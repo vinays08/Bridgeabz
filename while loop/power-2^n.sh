4e4573049f4286a1ba66ffffda1c28ef2f328bb4
vinays08 <94815005+vinays08@users.noreply.github.com>
2021-11-24 05:43:11 +0530

    Update power-2^n.sh

--- while loop/power-2^n.sh
+++ while loop/power-2^n.sh
@@ -1,10 +1,7 @@
-read -p "Enter the nth number:"  number
+read -p "Enter the number to print power of 2: "  number
 
 counter=0
-
 result=0
-
-
 while (($counter<= $number &&  $result < 256))
 do
 	result=$((2**counter))
