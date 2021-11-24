bff6a222c705b078502f75c34fc041f38be001b5
vinays08 <94815005+vinays08@users.noreply.github.com>
2021-11-24 06:14:06 +0530

    Update gambler.sh

--- while loop/gambler.sh
+++ while loop/gambler.sh
@@ -1,17 +1,18 @@
 gambler_money=100
-is_bet=1 
+
 
 bet_times=0
 bet_wons=0
 bet_lose=0
 
-while [ $gambler_money -gt 0 ] && [ $gambler_money -lt 200 ] && [ $is_bet -eq 1 ]
+while [ $gambler_money -gt 0 ] && [ $gambler_money -lt 200 ]
 do
 
 	flip_coin=$((RANDOM%2))
 	
 	if [ $flip_coin -eq 0 ] 
-	then
+	then 
+		gambler_money=$((gambler_money+1))
 		bet_wons=$((bet_wons+1))
 	else
 		gambler_money=$((gambler_money-1))
